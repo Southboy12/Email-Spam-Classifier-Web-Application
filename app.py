cd6e3a8300f810c826504de66c9e6166ed2cb64b
Southboy12 <menkachi85@gmail.com>
2023-07-09 07:27:25 +0100

    Added comments to every block of code for easy understanding

--- app.py
+++ app.py
@@ -4,21 +4,24 @@ import pickle
 
 # Create an instance of the Flass class
 app = Flask(__name__)
-cv = pickle.load(open("models/cv.pkl", 'rb'))
-clf = pickle.load(open("models/clf.pkl", 'rb'))
 
-# Register a route
+# Load the trained models
+cv = pickle.load(open("models/cv.pkl", 'rb'))  # Load the CountVectorizer model
+clf = pickle.load(open("models/clf.pkl", 'rb'))  # Load the classifier model
+
+# Register a route for the home page
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html')   # Render the HTML template for the home page
 
+# Register a route for the prediction endpoint
 @app.route("/predict", methods=["POST"])
 def predict():
-    email = request.form.get('content')
-    tokenized_email = cv.transform([email])
-    prediction = clf.predict(tokenized_email)
-    prediction = 1 if prediction == 1 else -1
-    return render_template("index.html", prediction=prediction, email=email)
+    email = request.form.get('content')   # Get the emil content from the form submitted in the HTML template
+    tokenized_email = cv.transform([email])   # Transform the email text using the CountVectorizer model
+    prediction = clf.predict(tokenized_email)   # Make a prediction using the classifier model
+    prediction = 1 if prediction == 1 else -1   # Adjust the prediction value if needed
+    return render_template("index.html", prediction=prediction, email=email)   # Render the HTML template with the prediction and email data
 
 # Run the flask application
 if __name__ == '__main__':
